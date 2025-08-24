431cb8ff94fdd240cbd4ae26ffef725e98999e65
Matt Nemitz <mattn@speechmatics.com>
2025-08-24 12:57:33 +0100

    Cloudflare setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
+import { cloudflare } from "@cloudflare/vite-plugin";
 import mdx from "@mdx-js/rollup";
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [mdx(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    cloudflare({ viteEnvironment: { name: "ssr" } }),
+    mdx(),
+    reactRouter(),
+    tsconfigPaths(),
+  ],
 });
